3d1da3ee2f029bebd5575cc804ff80c8f1242e6a
Andi Miller <andi@andimiller.net>
2019-02-20 16:23:51 +0000

    try adding tracer to apply

--- tool/src/main/scala/io/idml/tool/IdmlToolConfig.scala
+++ tool/src/main/scala/io/idml/tool/IdmlToolConfig.scala
@@ -1,3 +1,3 @@
 import java.io.File
 
-case class IdmlToolConfig(files: List[File] = List.empty, pretty: Boolean = false, unmapped: Boolean = false, strict: Boolean = false)
+case class IdmlToolConfig(files: List[File] = List.empty, traceOutput: Option[File] = None, pretty: Boolean = false, unmapped: Boolean = false, strict: Boolean = false)
--- tool/src/main/scala/io/idml/tool/IdmlTools.scala
+++ tool/src/main/scala/io/idml/tool/IdmlTools.scala
@@ -15,12 +15,13 @@ import io.idml.hashing.HashingFunctionResolver
 import io.idml.jsoup.JsoupFunctionResolver
 import io.idml.server.Server
 import org.slf4j.LoggerFactory
+
 import scala.collection.JavaConverters._
 import org.http4s.server.blaze.BlazeBuilder
 import io.idml.server.WebsocketServer
+import io.idml.utils.Tracer.Annotator
 
 import scala.util.{Failure, Success, Try}
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object IdmlTools {
@@ -57,29 +58,27 @@ object IdmlTools {
     val pretty   = Opts.flag("pretty", "Enable pretty printing of output", short = "p").orFalse
     val unmapped = Opts.flag("unmapped", "This probably doesn't do what you think it does", short = "u").orFalse
     val strict   = Opts.flag("strict", "Enable strict mode", short = "s").orFalse
+    val trace    = Opts.option[File]("tracefile", "tracing output file").orNone
     val file     = Opts.arguments[File]("mapping file").orEmpty
 
     val log = LoggerFactory.getLogger("idml-tool")
 
-    (pretty, unmapped, strict, file).mapN { (p, u, s, f) =>
-      val config = new IdmlToolConfig(f, p, s, u)
+    (pretty, unmapped, strict, trace, file).mapN { (p, u, s, t, f) =>
+      val config = new IdmlToolConfig(f, t, p, s, u)
+      val annotator = t.map(_ => new Annotator)
 
-      val ptolemy = if (config.unmapped) {
+      val ptolemy =
         new Ptolemy(
           new PtolemyConf,
-          List[PtolemyListener](new UnmappedFieldsFinder).asJava,
+          List[Option[PtolemyListener]](
+            if (config.unmapped) Some(new UnmappedFieldsFinder) else None,
+            annotator
+          ).flatten.asJava,
           new StaticFunctionResolverService(
             (StaticFunctionResolverService.defaults.asScala ++ List(new JsoupFunctionResolver(),
                                                                     new HashingFunctionResolver())).asJava)
         )
-      } else {
-        new Ptolemy(
-          new PtolemyConf,
-          new StaticFunctionResolverService(
-            (StaticFunctionResolverService.defaults.asScala ++ List(new JsoupFunctionResolver(),
-                                                                    new HashingFunctionResolver())).asJava)
-        )
-      }
+
       val (found, missing) = config.files.partition(_.exists())
       missing.isEmpty match {
         case false =>
@@ -88,6 +87,7 @@ object IdmlTools {
           }
           sys.exit(1)
         case true =>
+          val strings = found.map(f => scala.io.Source.fromFile(f).mkString)
           val maps  = found.map(f => ptolemy.fromFile(f.getAbsolutePath))
           val chain = ptolemy.newChain(maps: _*)
           if (config.strict) {
@@ -105,6 +105,11 @@ object IdmlTools {
             }
             .foreach {
               case Success(json) =>
+                (t, annotator).tupled.foreach { case (traceFile, annotatorModule) =>
+                  if (maps.size == 1) {
+                    reflect.io.File(traceFile).writeAll(annotatorModule.render(strings.head))
+                  }
+                }
                 config.pretty match {
                   case true  => println(PtolemyJson.pretty(json))
                   case false => println(PtolemyJson.compact(json))
