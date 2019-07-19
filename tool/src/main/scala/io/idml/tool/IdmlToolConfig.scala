579bae8f16a4947a6460b21dd5abf42d5b33a954
Andi Miller <andi@andimiller.net>
2019-07-19 14:29:10 +0100

    Revamp the `apply` tool to add tracing, do a few bugfixes

--- tool/src/main/scala/io/idml/tool/IdmlToolConfig.scala
+++ tool/src/main/scala/io/idml/tool/IdmlToolConfig.scala
@@ -1,3 +1,7 @@
 import java.io.File
 
-case class IdmlToolConfig(files: List[File] = List.empty, pretty: Boolean = false, unmapped: Boolean = false, strict: Boolean = false)
+case class IdmlToolConfig(files: List[File] = List.empty,
+                          pretty: Boolean = false,
+                          unmapped: Boolean = false,
+                          strict: Boolean = false,
+                          traceFile: Option[File] = None)
