6ef86a538f4dcec687791e2c1825dfb22e2ffa57
Andi Miller <andi@andimiller.net>
2019-09-03 16:20:10 +0100

    Always use UTF-8 for UUID serializing

--- core/src/main/scala/io/idml/functions/json/UUIDModuleJson.scala
+++ core/src/main/scala/io/idml/functions/json/UUIDModuleJson.scala
@@ -1,6 +1,6 @@
 package io.idml.functions.json
 
-import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets.UTF_8
 import java.util.UUID
 
 import io.idml.datanodes.IString
@@ -12,8 +12,8 @@ class UUIDModuleJson(json: IdmlJson) extends ObjectModuleJson(json) {
 
   val uuid3Function: IdmlFunction0 = new IdmlFunction0 {
     override protected def apply(cursor: IdmlValue): IdmlValue = cursor match {
-      case o: IdmlObject => IString(UUID.nameUUIDFromBytes(serialize(o).toStringOption.get.getBytes(Charset.defaultCharset())).toString)
-      case n: IdmlString => IString(UUID.nameUUIDFromBytes(n.value.getBytes(Charset.defaultCharset())).toString)
+      case o: IdmlObject => IString(UUID.nameUUIDFromBytes(serialize(o).toStringOption.get.getBytes(UTF_8)).toString)
+      case n: IdmlString => IString(UUID.nameUUIDFromBytes(n.value.getBytes(UTF_8)).toString)
       case _             => CastUnsupported
     }
     override def name: String = "uuid3"
@@ -22,8 +22,8 @@ class UUIDModuleJson(json: IdmlJson) extends ObjectModuleJson(json) {
   val uuid5Function: IdmlFunction0 = new IdmlFunction0 {
     override protected def apply(cursor: IdmlValue): IdmlValue = cursor match {
       case o: IdmlObject =>
-        IString(IdmlUUID.nameUUIDFromBytes5(serialize(o).toStringOption.get.getBytes(Charset.defaultCharset())).toString)
-      case n: IdmlString => IString(IdmlUUID.nameUUIDFromBytes5(n.value.getBytes(Charset.defaultCharset())).toString)
+        IString(IdmlUUID.nameUUIDFromBytes5(serialize(o).toStringOption.get.getBytes(UTF_8)).toString)
+      case n: IdmlString => IString(IdmlUUID.nameUUIDFromBytes5(n.value.getBytes(UTF_8)).toString)
     }
     override def name: String = "uuid5"
   }
