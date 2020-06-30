b8f954e3a0ad37654155029c1415a782377c078f
Andi Miller <andi@andimiller.net>
2020-06-30 16:08:43 +0100

    Drop support for `admin1` and `city` lookups
    These required external databases, and I'd rather not have side-effecting like that in IDML.
    
    The timezone and ISO lookups are still available because they're small enough to ship with the JVM or with the jar sensibly.

--- geo/src/main/scala/io/idml/geo/GeoFunctionResolver.scala
+++ geo/src/main/scala/io/idml/geo/GeoFunctionResolver.scala
@@ -43,40 +43,7 @@ class GeoFunctionResolver(json: IdmlJson) extends FunctionResolver {
     IdmlFunctionMetadata("geo", List("lat"         -> "latitude", "long" -> "longitude"), "create a geo object from a lat and a long"),
     IdmlFunctionMetadata("country", List("country" -> "country code to look up"), "look up a country code"),
     IdmlFunctionMetadata("region", List("region"   -> "region name to look up"), "look up a region"),
-    IdmlFunctionMetadata("city", List("city"       -> "city name to look up"), "look up a city"),
-    IdmlFunctionMetadata("admin1", List("admin1"   -> "admin1 name to look up"), "look up an admin1 area"),
     IdmlFunctionMetadata("timezone", List.empty, "turn this geo into a timezone, eg. Europe/London")
   )
-}
-
-class GeoDatabaseFunctionResolver
-    extends InnerGeoDatabaseFunctionResolver(
-      driver = System.getenv("IDML_GEO_DB_DRIVER"),
-      cityUrl = System.getenv("IDML_GEO_CITY_JDBC_URL"),
-      admin1Url = System.getenv("IDML_GEO_ADMIN1_JDBC_URL"),
-      user = Option(System.getenv("IDML_GEO_DB_USER")).getOrElse(""),
-      password = Option(System.getenv("IDML_GEO_DB_PASSWORD")).getOrElse("")
-    )
-
-class InnerGeoDatabaseFunctionResolver(driver: String, cityUrl: String, admin1Url: String, user: String, password: String)
-    extends FunctionResolver {
-  val cityFunction   = Option(driver).map(_ => new CityFunction(driver, cityUrl, user, password))
-  val admin1Function = Option(driver).map(_ => new Admin1Function(driver, admin1Url, user, password))
-
-  override def providedFunctions(): List[IdmlFunctionMetadata] =
-    List(
-      IdmlFunctionMetadata("city", List("city"     -> "city name to look up"), "look up a city"),
-      IdmlFunctionMetadata("admin1", List("admin1" -> "admin1 name to look up"), "look up an admin1 area")
-    )
-
-  override def resolve(name: String, args: List[Argument]): Option[IdmlFunction] = {
-    (name, args) match {
-      case ("city", (city: Pipeline) :: Nil) =>
-        cityFunction.map(_.CityFunction(city))
-      case ("admin1", (admin1: Pipeline) :: Nil) =>
-        admin1Function.map(_.Admin1Function(admin1))
-      case _ => None
-    }
-  }
 
 }
