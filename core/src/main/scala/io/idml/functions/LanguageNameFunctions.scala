8fff3a90b97e5d91773c9a3f034c58bbdc36c6b0
Andi Miller <andi@andimiller.net>
2022-03-10 13:10:45 +0000

    Add `languageName` function to core (#32)
    * Add `languageName` function to core
    
    Adds two new functions:
    * `input.languageName()` on an expression will look the input up as an ISO language code, and find the name of the language in the JVM's default locale
    * `input.languageName("en")` on an expression would look the input up as an ISO language code, and find the name of the language in the described target locale
    
    Both functions are in `core` since they just rely on JVM internals, and are available in `BuiltinFunctionResolver`
    Tests have been added in `LanguageNameSuite.json`
    
    Note that `java.util.Locale` can just return the input if it doesn't recognise it, so we intercept that and turn it into a `MissingField`.
    If you'd like that behaviour please use `(input.languageName() | input)` to fall back to the input.
    
    * add `languageName` test for 3-char input
    
    * Add a test for chinese output, and fix a comment

---
+++ core/src/main/scala/io/idml/functions/LanguageNameFunctions.scala
@@ -0,0 +1,39 @@
+package io.idml.functions
+import io.idml.ast.Pipeline
+import io.idml.datanodes.IString
+import io.idml.{IdmlString, IdmlValue, InvalidCaller, InvalidParameters, MissingField}
+
+import java.util.Locale
+
+object LanguageNameFunctions {
+
+  private def lookupLanguageWithLocale(code: String, targetLocale: Option[String]): IdmlValue = {
+    val lang = Locale.forLanguageTag(code)
+    targetLocale.fold(lang.getDisplayLanguage)(t => lang.getDisplayLanguage(Locale.forLanguageTag(t))) match {
+      // Java can just return the original input if it didn't know, and we'll blank that out
+      // if you do want the java behaviour, use (code.languageName() | code)
+      case result if result == code => MissingField
+      case result => IString(result)
+    }
+  }
+
+  object LanguageNameFunction0 extends IdmlFunction0 {
+    override protected def apply(cursor: IdmlValue): IdmlValue = cursor match {
+      case s: IdmlString =>  lookupLanguageWithLocale(s.value, None)
+      case _ => InvalidCaller
+    }
+
+    override def name: String = "languageName"
+  }
+
+  case class LanguageNameFunction1(arg: Pipeline) extends IdmlFunction1 {
+    override protected def apply(cursor: IdmlValue, targetLocale: IdmlValue): IdmlValue = (cursor, targetLocale) match {
+      case (s: IdmlString, t: IdmlString) => lookupLanguageWithLocale(s.value, Some(t.value))
+      case (_: IdmlString, _) => InvalidParameters
+      case _ => InvalidCaller
+    }
+
+    override def name: String = "languageName"
+  }
+
+}
